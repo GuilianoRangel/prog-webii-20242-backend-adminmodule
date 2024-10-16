6219af8be75396a2b8a96234eba1919d49007637
Guiliano Rangel <guiliano@gmail.com>
2024-10-16 19:29:27 -0300

    [progwebii-14] ADD - Criação Projeto AdminModule

---
+++ src/main/java/br/ueg/progweb2/arquitetura/adminmodule/ProgWebi20242BackendAdminmoduleApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.progweb2.arquitetura.adminmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgWebi20242BackendAdminmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgWebi20242BackendAdminmoduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ueg/progweb2/arquitetura/adminmodule/ProgWebi20242BackendAdminmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.progweb2.arquitetura.adminmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgWebi20242BackendAdminmoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
